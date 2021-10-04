3739dd0fb56787edd67bc778f43df5fe833f48c1
Slavoljub.Petkovic <slavoljub.petkovic@outlook.com>
2021-10-03 23:59:37 -0400

    Pandas

--- LiveStream.py
+++ LiveStream.py
@@ -1,13 +1,12 @@
 # Import Dependencies
-import sqlite3
-import time
+
 import board
+import pandas as pd
 from busio import I2C
 import adafruit_bme680
 from datetime import datetime, timedelta
 import time
 import requests
-
 import mysql.connector
 import json
 import sqlalchemy
@@ -16,16 +15,17 @@ import sqlalchemy
 with open("config.json") as config:
     param = json.load(config)
 
-# Create library object using our Bus I2C port
+# Create library object using Bus I2C port
 i2c = I2C(board.SCL, board.SDA)
 bme680 = adafruit_bme680.Adafruit_BME680_I2C(i2c, debug=False)
 
 # change this to match the location's pressure (hPa) at sea level
 bme680.sea_level_pressure = 1013.25
 
-
-# Using while loop capture the data in variables and store it in database
+# Read data from sensors
 while True:
+
+    data = pd.DataFrame()
     # Create the now variable to capture the current moment
     TimeStamp = datetime.now()
     Temperature = round((bme680.temperature * 9/5) + 32, 2)
@@ -34,6 +34,20 @@ while True:
     Pressure = round(bme680.pressure, 2)
     Altitude = round(bme680.altitude, 2)
 
+
+    # Adding collected measurements into dataframe
+    data = [
+        {
+            "TimeStamp": now,
+            "Temperature": Temperature,
+            "Gas": Gas,
+            "Humidity": Humidity,
+            "Pressure": Pressure,
+            "Altitude": Altitude
+        }
+    ]
+
+    # Try establishing connection with database
     try:
         engine = sqlalchemy.create_engine('mysql+mysqlconnector://{0}:{1}@{2}/{3}'.
                                           format(param['MyDemoServer'][0]['user'],
@@ -45,13 +59,15 @@ while True:
         db_con = engine.connect()
         if db_con.connect():
             try:
-                sql = """INSERT INTO sensordata (TimeStamp, Temperature, Gas, Humidity, Pressure, Altitude)
-                            VALUES (%s, %s, %s, %s, %s, %s) """
+                # SQL query that will insert collected data into sensordata table
+                #sql = """INSERT INTO sensordata (TimeStamp, Temperature, Gas, Humidity, Pressure, Altitude)
+                #           VALUES (%s, %s, %s, %s, %s, %s) """
                 # Establish the record with set of data to be taken form variables
-                record = (TimeStamp, Temperature, Gas, Humidity, Pressure, Altitude)
+                #record = (TimeStamp, Temperature, Gas, Humidity, Pressure, Altitude)
                 # Execute sql with collected records
-                db_con.execute(sql, record)
+                #db_con.execute(sql, record)
                 # Close connection
+                data.to_sql('sensordata', con = db_con, if_exist = 'append', index = False)
                 db_con.close()
                 # Dispose the engine
                 engine.dispose()
@@ -60,7 +76,7 @@ while True:
     except OSError as e:
         print(e)
 
-# API Post
+    # Power BI API
 
     url = 'https://api.powerbi.com/beta/94cd2fa9-eb6a-490b-af36-53bf7f5ef485/datasets/2a7a2529-dbfd-4c32-9513-7d5857b61137/rows?noSignUpCheck=1&key=nS3bP1Mo4qN9%2Fp6XJcTBgHBUV%2FcOZb0edYrK%2BtVWDg6iWwzRtY16HWUGSqB9YsqF3GHMNO2fe3r5ltB7NhVIvw%3D%3D'
 
@@ -68,17 +84,6 @@ while True:
         datetime.now(),
         "%Y-%m-%dT%H:%M:%S"
     )
-
-    data = [
-        {
-            "TimeStamp": now,
-            "Temperature": Temperature,
-            "Gas": Gas,
-            "Humidity": Humidity,
-            "Pressure": Pressure,
-            "Altitude": Altitude
-        }
-    ]
 
     # post/push data to the streaming API
     headers = {
@@ -91,6 +96,7 @@ while True:
         data=json.dumps(data)
     )
 
+    # Re-run the script at the beginning of every new minute.
     dt = datetime.now() + timedelta(minutes=1)
     dt = dt.replace(second=1)
 
