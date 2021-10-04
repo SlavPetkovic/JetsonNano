1dbc19d1960eecaeb298e2e62f2be204a8e3b213
Slavoljub.Petkovic <slavoljub.petkovic@outlook.com>
2021-10-04 02:47:26 -0400

    main.py

--- main.py
+++ main.py
@@ -7,15 +7,16 @@ def etl(name):
     data = sensorsreading()
     dataload(data)
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    etl('PyCharm')
-    # Re-run the script at the beginning of every new minute.
     dt = datetime.now() + timedelta(minutes=1)
     dt = dt.replace(second=1)
 
     while datetime.now() < dt:
         time.sleep(1)
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    etl('PyCharm')
+    # Re-run the script at the beginning of every new minute.
+
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
